b5f9b71871f719ce6108ea6c4a61e149526da0b8
Sreya Tuli <tup54175@temple.edu>
2025-11-19 18:07:49 -0500

    deleted some suplicate files

--- src/main/java/com/example/service/ApplicationRegistrationService.java
+++
@@ -1,8 +0,0 @@
-package com.example.service;
-
-import com.example.bindings.USACitizenApplication;
-
-public interface ApplicationRegistrationService {
-	public Integer USRegistrationForm (USACitizenApplication inputs);
-
-}
